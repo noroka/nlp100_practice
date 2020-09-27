631e8bc0945c68cdd1ab4e663871ebb825c448f9
noroka <noro.kclass0325@gmail.com>
2020-09-27 10:12:18 +0900

    part7の追加

---
+++ util/.ipynb_checkpoints/Downloader-checkpoint.py
@@ -0,0 +1,16 @@
+import os
+import pprint
+import time
+import urllib.error
+import urllib.request
+
+# ファイルのダウンロード
+def download_file(url, dst_path):
+    try:
+        with urllib.request.urlopen(url) as web_file, open(dst_path, 'wb') as local_file:
+            local_file.write(web_file.read())
+    except urllib.error.URLError as e:
+        print(e)
+
+def download_file_to_dir(url, dst_dir):
+    download_file(url, os.path.join(dst_dir, os.path.basename(url)))
